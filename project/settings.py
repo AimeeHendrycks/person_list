1d46b1a0cc929d9375bf47eec69b78310e9e553d
AimeeH <am.hendrycks@gmail.com>
2015-12-07 12:08:05 -0700

    changed db

--- project/settings.py
+++ project/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'person_list',
+        'HOST':'127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '',
     }
 }
 
