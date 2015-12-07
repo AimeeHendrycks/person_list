466047e0755f904b6db5a64dffb35ede597c5e66
AimeeH <am.hendrycks@gmail.com>
2015-12-07 12:02:43 -0700

    added static root

--- project/settings.py
+++ project/settings.py
@@ -83,4 +83,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/main/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
